1008c8a5393210e760d4c0a5343e6c4fa6cb7a5c
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-02-28 13:17:20 -0600

    basic functional navbar in place

---
+++ src/components/Journey.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
+import { Login } from "./auth/Login"
+// import { Register } from "./auth/Register"
+
+export const Journey = () => (
+  <>
+    <Route render={() => {
+            return (
+                <>
+                    <Route render={props => <NavBar {...props} />} />
+                    <Route render={props => <ApplicationViews {...props} />} />
+                </>
+            )
+    }} />
+
+    <Route path="/login" render={props => <Login {...props} />} />
+    {/* <Route path="/register" render={props => <Register {...props} />} /> */}
+  </>
+
+)
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Link, NavLink, useHistory } from 'react-router-dom'
+import './NavBar.scss'
+// import Logo from "./r-logo.jpg"
+
+export const NavBar = () => {
+    const history = useHistory()
+    
+
+    return (
+        <ul className="navbar">
+            <li className="navbar__item">
+                {/* <img alt="Rare Logo" className="navbar__logo" src={Logo} /> */}
+                <h1 style={{ textAlign: "center", fontSize: "2em", marginTop: "0" }}>It Takes A Village</h1>
+            </li>
+            <li className="navbar__item">
+                <NavLink className="navbar__link" to="/">Home</NavLink>
+            </li>
+            <li className="navbar__item">
+                <NavLink className="navbar__link" to="/resources">Resources</NavLink>
+            </li>
+            {
+                (localStorage.getItem("journey_token") !==null) ? 
+                    <li className="navbar__item">
+                        <NavLink className="navbar__link" to="/tutoring-signup">Tutoring Signup</NavLink>
+                    </li> :
+                    <li className="navbar__item">
+                        <NavLink className="navbar__link" to="/login">Tutoring Signup</NavLink>
+                    </li>
+            }
+            {
+                (localStorage.getItem("journey_token") !==null) ? 
+                    <li className="navbar__item">
+                        <NavLink className="navbar__link" to="/profile">Profile</NavLink>
+                    </li> :
+                    ""
+            }
+            {
+                (localStorage.getItem("journey_token") !==null) ? 
+                    <li className="navbar__item">
+                        <NavLink className="navbar__link" to="/profile">Profile</NavLink>
+                    </li> :
+                    ""
+            }
+            {
+                (localStorage.getItem("journey_token") !== null) ?
+                    <li className="navbar__item">
+                        <button className="navbar__link logout"
+                            onClick={() => {
+                                localStorage.removeItem("journey_token")
+                                history.push({ pathname: "/" })
+                            }}
+                        >Logout</button>
+                    </li> :
+                    ""
+            }        
+            </ul>
+    )
+}
