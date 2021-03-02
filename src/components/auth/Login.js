d6eebe8844ce5c09bdcbf72dc3af1f61bd3d92f9
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-03-01 18:54:34 -0600

    user can log in or register account

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,66 @@
 import React from "react"
+import { Link, useHistory } from "react-router-dom"
+// import "./Auth.css"
+
 
 export const Login = () => {
-  return (
-    <h1>Login</h1>
-  )
+    const email = React.createRef()
+    const password = React.createRef()
+    const invalidDialog = React.createRef()
+    const history = useHistory() 
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        return fetch("http://127.0.0.1:8000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                username: email.current.value,
+                password: password.current.value
+            })
+        })
+            .then(res => res.json())
+            .then(res => {
+                if ("valid" in res && res.valid && "token" in res) {
+                    localStorage.setItem( "journey_token", res.token )
+                    localStorage.setItem( "journey_user_id", res.user_id )
+                    history.push("/")
+                }
+                else {
+                    invalidDialog.current.showModal()
+                }
+            })
+    }
+
+    return (
+        <main className="container--login">
+            <dialog className="dialog dialog--auth" ref={invalidDialog}>
+                <div>Email or password was not valid.</div>
+                <button className="button--close" onClick={e => invalidDialog.current.close()}>Close</button>
+            </dialog>
+            <section className="login-section">
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h2>Please log in to access the tutoring signup</h2>
+                    <fieldset>
+                        <input ref={email} type="email" id="email" className="form-control"  placeholder="Email address" required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <input ref={password} type="password" id="password" className="form-control"  placeholder="Password" required />
+                    </fieldset>
+                    <fieldset style={{
+                        textAlign:"center"
+                    }}>
+                        <button className="login-button" type="submit">Login</button>
+                    </fieldset>
+                </form>
+            </section>
+            <section>
+                <Link to="/register" className="link--register">Click here to register a new account.</Link>
+            </section>
+        </main>
+    )
 }
