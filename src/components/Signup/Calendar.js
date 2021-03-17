e7dea454b10147e6af82fc5797674f773defb3f7
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-03-16 21:38:17 -0500

    link to calendar data export + styling

--- src/components/Signup/Calendar.js
+++ src/components/Signup/Calendar.js
@@ -1,21 +1,27 @@
 import React, { useContext, useEffect } from "react"
 import { CalContext } from "./CalendarProvider"
+import { UserContext } from "../Users/UserProvider"
 import "./Calendar.scss"
 
 
 export const Calendar = ({history}) => {
 
   const { calTexts, getCalText } = useContext(CalContext)
+  const { users, getUserById } = useContext(UserContext)
+
+  const userId = parseInt(localStorage.getItem("journey_user_id"))
 
   useEffect(() => {
     getCalText()
+    getUserById(userId)
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
     <>
     <h1>Calendar</h1>
-    <div>
+    <div className="cal-page">
+    <div className="scripts-button">{users.is_staff === true ? <button className="scripts"><a href="https://docs.google.com/spreadsheets/d/1oYpy_R5FkckHzmVnRl_QKEBfa-wfx_MWe4sBnFwA48A/edit?usp=sharing" target="blank">Export Calendar Data</a></button> : ""}</div>
       <div className="calendar-info">
         {
           calTexts.map(calText => {
@@ -24,8 +30,9 @@ export const Calendar = ({history}) => {
                 </section>            
           })
         }<i className="far fa-edit fa-lg" onClick={() => history.push("/update-text")}></i></div>
-      <p>If the calendar on the page isn't displaying, you can view tutoring slots <a href="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UU9NdExVa29Ya24zfGRlZmF1bHR8MDVkZDBkMTQ4NDhlNDYzMDA5NWE1YmNmOWRiYmYyMjA" target="blank">here</a></p>
-      <iframe title="calendar" src="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UU9NdExVa29Ya24zfGRlZmF1bHR8MDVkZDBkMTQ4NDhlNDYzMDA5NWE1YmNmOWRiYmYyMjA" height="600" width="800"></iframe>
+      <p>Please track your volunteer hours <a className="hours" href="https://www.cognitoforms.com/LMV1/VolunteerHours" target="blank">here</a> at least once a month.</p>
+      <p>If the calendar on the page isn't displaying, you can view tutoring slots <a className="google-cal" href="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UU9NdExVa29Ya24zfGRlZmF1bHR8MDVkZDBkMTQ4NDhlNDYzMDA5NWE1YmNmOWRiYmYyMjA" target="blank">here</a>.</p>
+      <div className="iframe"><iframe title="calendar" src="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UU9NdExVa29Ya24zfGRlZmF1bHR8MDVkZDBkMTQ4NDhlNDYzMDA5NWE1YmNmOWRiYmYyMjA" height="600" width="900"></iframe></div>
     </div>
     </>
   )
