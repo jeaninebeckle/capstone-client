4b830944430b0c2cf93dbea7b5cb077dc0d0b1a2
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-03-20 12:17:09 -0500

    change login/reg urls

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -26,7 +26,7 @@ export const Register = () => {
                 "display_name": displayName.current.value,
             }
 
-            return fetch("http://127.0.0.1:8000/register", {
+            return fetch("https://lmv-journey.herokuapp.com/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
